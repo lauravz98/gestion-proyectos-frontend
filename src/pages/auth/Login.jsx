76f9e6103956d88cc6ddca4ace1b16e0dd935db8
lauravz98 <75417516+lauravz98@users.noreply.github.com>
2021-12-10 12:04:58 +0100

    conexion entre back front se envia el token correctamente

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -9,60 +9,62 @@ import { useAuth } from "context/authContext";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    // const navigate = useNavigate();
-    // const { setToken } = useAuth();
-    const { form, formData, updateFormData } = useFormData();
+  // const navigate = useNavigate();
+  // const { setToken } = useAuth();
+  const { form, formData, updateFormData } = useFormData();
 
-    // const [
-    //     login,
-    //     { data: dataMutation, loading: mutationLoading, error: mutationError },
-    // ] = useMutation(LOGIN);
+  const [
+    login,
+    { data: dataMutation, loading: mutationLoading, error: mutationError },
+  ] = useMutation(LOGIN);
 
-    const submitForm = (e) => {
-        e.preventDefault();
+  const submitForm = (e) => {
+    e.preventDefault();
 
-        // login({
-        // variables: formData,
-        // });
-    };
+    login({
+      variables: formData,
+    });
+  };
 
-    // useEffect(() => {
-    //     console.log("data mutation", dataMutation);
+    useEffect(() => {
+        if (dataMutation) {
+            console.log("data mutation", dataMutation);
+        }
     //     if (dataMutation) {
     //     if (dataMutation.login.token) {
     //         setToken(dataMutation.login.token);
     //         navigate("/");
     //     }
     //     }
-    // }, [dataMutation, setToken, navigate]);
+  }, [dataMutation]); //, setToken, navigate
 
-    return (
-        <div className="flex flex-col items-center justify-center w-full h-full p-10">
-        <h1 className="text-xl font-bold text-gray-900">Iniciar sesión</h1>
-        <form
-            className="flex flex-col"
-            onSubmit={submitForm}
-            onChange={updateFormData}
-            ref={form}
-        >
-            <Input name="correo" type="email" label="Correo" required={true} />
-            <Input
-            name="password"
-            type="password"
-            label="Contraseña"
-            required={true}
-            />
-            <ButtonLoading
-            disabled={Object.keys(formData).length === 0}
-            loading={false}
-            text="Iniciar Sesión"
-            />
-        </form>
-        <span>¿No tienes una cuenta?</span>
-        <Link to="/auth/register">
-            <span className="text-blue-700">Regístrate</span>
-        </Link>
-        </div>
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full p-10">
+      <h1 className="text-xl font-bold text-gray-900">Iniciar sesión</h1>
+      <form
+        className="flex flex-col"
+        onSubmit={submitForm}
+        onChange={updateFormData}
+        ref={form}
+      >
+        <Input name="correo" type="email" label="Correo" required={true} />
+        <Input
+          name="password"
+          type="password"
+          label="Contraseña"
+          required={true}
+        />
+        <ButtonLoading
+          disabled={Object.keys(formData).length === 0}
+          loading={mutationLoading}
+          text="Iniciar Sesión"
+        />
+      </form>
+      <span>¿No tienes una cuenta?</span>
+      <Link to="/auth/register">
+        <span className="text-blue-700">Regístrate</span>
+      </Link>
+    </div>
   );
 };
 
